80d117d108013ca1ab94e2b40a71101e48f9b8cf
choijudy <128137318+choijudy@users.noreply.github.com>
2025-03-15 14:16:25 +0900

    [Bronze III] Title: 팩토리얼, Time: 96 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int f = 1;
+
+        for(int i = n; i>0; i--){
+            f *= i;
+        }
+
+        System.out.println(f);
+    }
+}
