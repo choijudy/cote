736eeea22618fd3adfc6543fd6e3aa9d13aa950d
choijudy <128137318+choijudy@users.noreply.github.com>
2025-03-15 14:47:00 +0900

    [Bronze V] Title: A+B - 4, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input;
+        
+        while ((input = br.readLine()) != null) { // EOF 처리
+            StringTokenizer st = new StringTokenizer(input);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println(a + b);
+        }
+    }
+}
