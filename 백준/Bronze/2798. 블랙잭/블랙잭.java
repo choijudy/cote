45071878e8352eceef721dba2b778d05d3848281
choijudy <128137318+choijudy@users.noreply.github.com>
2025-05-02 22:44:14 +0900

    [Bronze II] Title: 블랙잭, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -2,13 +2,35 @@ import java.util.*;
 import java.io.*;
 
  class Main {
+     static int n;
+     static int m;
+     static int[] arr;
+     static int max = 0;
+
+     public static void dfs(int s, int cnt, int sum){
+         // 종료조건 1
+         if(sum > m){
+             return;
+         }
+         // 종료조건 2
+         else if(cnt == 3){
+             max = Math.max(max, sum);
+             return;
+         }
+
+         for(int i=s; i<n; i++){
+             dfs(i+1, cnt+1, sum + arr[i]);
+         }
+
+     }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n];
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
 
         st = new StringTokenizer(br.readLine());
 
@@ -16,20 +38,8 @@ import java.io.*;
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int max = 0;
-
-        for (int i = 0; i < n - 2; i++) {
-            for (int j = i + 1; j < n - 1; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    int sum = arr[i] + arr[j] + arr[k];
-                    if (max <= sum && sum <= m) {
-                        max = sum;
-                    }
-                }
-            }
-        }
+        dfs(0, 0, 0);
 
         System.out.println(max);
-
     }
 }
