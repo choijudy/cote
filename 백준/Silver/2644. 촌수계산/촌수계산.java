09427779342f3b58ac081ca24ff9f249d2cb2178
choijudy <128137318+choijudy@users.noreply.github.com>
2025-06-02 20:29:03 +0900

    [Silver II] Title: 촌수계산, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -4,59 +4,88 @@ import java.util.*;
 public class Main {
     static int n;
     static int m;
+    static ArrayList<Integer>[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        // 전체 사람의 수 n
         n = Integer.parseInt(br.readLine());
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // 촌수 계산 할 두 사람 a,b
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
 
+        // 관계의 개수 (노드 간선 수) m
         m = Integer.parseInt(br.readLine());
 
-        int[][] graph = new int[n + 1][n + 1];
+        // 부모 자식간의 관계 배열
+        arr = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = new ArrayList<Integer>();
+        }
+
+        // 입력 받아서 저장할 배열
+        int[][] edges = new int[m][2];
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
 
-            graph[x][y] = 1;
-            graph[y][x] = 1;
+            edges[i][0] = Integer.parseInt(st.nextToken());
+            edges[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(bfs(a, b, graph));
+        // 입력받은 배열을 arr에 노드처럼 저장하기
+        for (int[] e : edges) {
+            int e1 = e[0]; // 부모 노드
+            int e2 = e[1]; // 자식 노드
+
+            // 양방향 링크로 수정
+            arr[e1].add(e2);
+            arr[e2].add(e1);
+        }
+
+        System.out.println(bfs(a, b));
+
     }
 
-    static int bfs(int a, int b, int[][] graph) {
-        Queue<int[]> q = new LinkedList<>();
-
+    static int bfs(int a, int b) {
+        // 해당 노드를 방문했는지 검사
+        // index를 노드 번호로 사용 (n + 1 크기)
         boolean[] visited = new boolean[n + 1];
 
-        visited[a] = true;
+        // 방문할 다음 노드를 저장할 queue 생성
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+
+        // 큐에 시작 노드 넣어줌, 거리는 0으로 설정
         q.add(new int[] { a, 0 });
 
         while (!q.isEmpty()) {
-            int[] temp = q.poll();
-            int now = temp[0];
-            int dist = temp[1];
+            // 현재 큐에 있는 값을 가져와서
+            // 현재 node와 거리를 저장해둠
+            int[] u = q.poll();
+            int node = u[0];
+            int dist = u[1];
 
-            if (now == b) {
+            // 현재 노드에 방문한 적 있는지 검사
+            if (visited[node])
+                continue;
+
+            // 현재 노드를 true로 바꿔줌
+            visited[node] = true;
+
+            // 현재 노드가 b 노드가 되면 거리 반환
+            if (node == b) {
                 return dist;
             }
 
-            for (int i = 1; i <= n; i++) {
-
-                if (graph[now][i] == 1) {
-                    if (!visited[i]) {
-                        visited[i] = true;
-                        q.add(new int[] { i, dist + 1 });
-                    }
-                }
-
+            // 해당 노드에서 방문할 다음 큐를 찾아서 넣음
+            for (int e : arr[node]) {
+                q.add(new int[] { e, dist + 1 });
             }
+
         }
 
         return -1;
